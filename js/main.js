a07eee95098c6fab1477ca9468b39f8a5146418d
Crystal <fhy_crystal@163.com>
2016-04-06 17:17:12 +0800

    change meta and script links

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
 $(document).ready(function() {
-	// 订单详情页顶部导航切换
-	var $nav_li = $('.top_nav li');
-	var $nav_con = $('.content .con_item');
-	$nav_li.click(function() {
-		$(this).siblings().removeClass('active');
-		$(this).addClass('active')
-		var data_nav = $(this).attr('data-nav');//top_nav li 的data-nav
-		$nav_con.each(function(i) {
-			if($(this).attr('data-con') == data_nav)
-			{
-				$(this).show();
-			}
-			else
-			{
-				$(this).hide();
-			}
-
-		});
-	});
-
 	//参团倒计时
 	timer(86399);
 });
@@ -36,7 +16,7 @@ function timer(time){
 			day = Math.floor(time / (24 * 60 * 60));
 			hour = Math.floor(time / (60 * 60) - (day * 24));
 			minute = Math.floor(time / 60 - (day * 24 *60) - (hour * 60));
-			second = Math.floor(time - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60));
+			second = Math.floor(time - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60));//Math.floor()向下取整
 		}
 		if(hour <= 9)
 		{
